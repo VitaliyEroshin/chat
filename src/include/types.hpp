738ee29e34d1ca94fc2dca8dacb505f5c144a4fd
vitaliy <vitaliy-eroshin@yandex.ru>
2022-03-19 23:15:36 +0300

    Moved all typedefs to types.hpp

---
+++ src/include/types.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+
+typedef u_int16_t userid_t;
+typedef u_int16_t chatid_t;
+typedef u_int16_t code_t;
+
+typedef u_int64_t objectid_t;
+
+typedef std::string login_t;
+typedef std::string password_t;
+typedef std::string object_message_t;
+
+typedef std::string output_t;
+typedef char output_char_t;
