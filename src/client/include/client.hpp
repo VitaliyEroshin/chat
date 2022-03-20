4ebfec61bcc9d3142a636e3eda7f24f6cb13149e
vitaliy <vitaliy-eroshin@yandex.ru>
2022-03-20 19:05:56 +0300

    Edited access modificators in Client class

--- src/client/include/client.hpp
+++ src/client/include/client.hpp
@@ -19,8 +19,7 @@ struct ObjectTree {
 class Client {
 private:
   bool setAddress(std::string ip, int port);
-  
-public:
+
   enum Status {
     offline, online, connecting, authentification, failed
   };
@@ -30,8 +29,6 @@ public:
   UserInterface ui;
   Encoder& encoder;
   ObjectTree data;
-
-  explicit Client(Encoder& encoder);
   
   void setupAddress();
   int connectToHost();
@@ -40,11 +37,14 @@ public:
   void refreshMessages();
   void sendText(const std::string& text);
 
-  int session();
   int connect();
   void showBackground(std::atomic<bool>& connecting);
   void listen();
 
   void readServer(std::atomic<bool>& run);
   void readUserInput(std::atomic<bool>& run);
+
+public:
+  explicit Client(Encoder& encoder);
+  int session();
 };
