09d451da6005cb8af82d9cc52e3499f9818944b9
vitaliy <vitaliy-eroshin@yandex.ru>
2022-04-08 23:21:40 +0300

    Added std::endl handling in logger

--- src/logger/logger.hpp
+++ src/logger/logger.hpp
@@ -1,22 +1,30 @@
 #pragma once
 #include <vector>
 #include <iostream>
+#include <ostream>
 
 class Logger {
 public:
-    Logger(std::initializer_list<std::ostream*> streams);
+  Logger(std::initializer_list<std::ostream*> streams);
 
-    template<typename T>
-    Logger& operator<<(T object);
-
-private:
-    std::vector<std::ostream*> s;
-};
-
-template<typename T>
-Logger& Logger::operator<<(T object) {
+  template<typename T>
+  Logger& operator<<(const T& object) {
     for (auto &out : s) {
-        *out << object;
+      *out << object;
     }
     return *this;
-}
+  }
+
+  typedef std::basic_ostream<char, std::char_traits<char> > outType;
+  typedef outType& (*endlManipulator)(outType&);
+
+  Logger& operator<<(endlManipulator manip) {
+    for (auto &out : s) {
+      manip(*out);
+    }
+    return *this;
+  }
+
+private:
+  std::vector<std::ostream*> s;
+};
