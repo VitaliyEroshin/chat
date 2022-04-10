7ba4cba1072f360383eee72a53173fde650f9d6c
vitaliy <vitaliy-eroshin@yandex.ru>
2022-04-10 20:08:24 +0300

    Added Storage interface methods (messages), commented RAMStorage as outdated

--- src/server/include/storage.hpp
+++ src/server/include/storage.hpp
@@ -26,45 +26,50 @@ public:
   virtual std::vector<userid_t> getUserFriends(userid_t id) = 0;
   virtual std::vector<chatid_t> getUserChats(userid_t id) = 0;
   virtual int addFriend(userid_t selfId, userid_t target) = 0;
+
+  virtual void setMessage(Object object, Encoder& encoder) = 0;
+  virtual void addMessage(Object object, Encoder& encoder, chatid_t chatid) = 0;
+  virtual Object getMessage(int id, Encoder& encoder) = 0;
+  virtual Object getLastMessage(Encoder& encoder, chatid_t chatid) = 0;
 };
 
-class RAMStorage: public Storage {
-  struct Chat {
-    explicit Chat(chatid_t id);
-    Chat();
+// class RAMStorage: public Storage {
+//   struct Chat {
+//     explicit Chat(chatid_t id);
+//     Chat();
 
-    chatid_t id;
-    objectid_t lastMessage;
-    std::set<userid_t> members;
-  };
+//     chatid_t id;
+//     objectid_t lastMessage;
+//     std::set<userid_t> members;
+//   };
 
-  std::map<chatid_t, Chat> chats;
-  std::map<userid_t, User> users;
-  std::map<std::string, userid_t> userids;
+//   std::map<chatid_t, Chat> chats;
+//   std::map<userid_t, User> users;
+//   std::map<std::string, userid_t> userids;
 
-  std::map<userid_t, chatid_t> currentChat;
-  std::map<userid_t, std::vector<userid_t>> friendsList;
-  std::map<userid_t, std::vector<chatid_t>> availableChats;
+//   std::map<userid_t, chatid_t> currentChat;
+//   std::map<userid_t, std::vector<userid_t>> friendsList;
+//   std::map<userid_t, std::vector<chatid_t>> availableChats;
 
-  userid_t generateUserId();
-  chatid_t generateChatId();
-  const User& getUserReference(userid_t id);
+//   userid_t generateUserId();
+//   chatid_t generateChatId();
+//   const User& getUserReference(userid_t id);
 
-public:
-  int getUser(const login_t& login, const password_t& password) override;
-  int addUser(const login_t& login, const password_t& password) override;
+// public:
+//   int getUser(const login_t& login, const password_t& password) override;
+//   int addUser(const login_t& login, const password_t& password) override;
 
-  int createChat(userid_t creator) override;
-  int inviteToChat(userid_t selfId, userid_t target, chatid_t chat) override;
-  chatid_t getChat(userid_t selfId) override;
+//   int createChat(userid_t creator) override;
+//   int inviteToChat(userid_t selfId, userid_t target, chatid_t chat) override;
+//   chatid_t getChat(userid_t selfId) override;
 
-  std::string getUserNickname(userid_t id) override;
+//   std::string getUserNickname(userid_t id) override;
 
-  int setUserChat(userid_t id, chatid_t chat) override;
-  std::vector<userid_t> getUserFriends(userid_t id) override;
-  std::vector<chatid_t> getUserChats(userid_t id) override;
-  int addFriend(userid_t selfId, userid_t target) override;
-};
+//   int setUserChat(userid_t id, chatid_t chat) override;
+//   std::vector<userid_t> getUserFriends(userid_t id) override;
+//   std::vector<chatid_t> getUserChats(userid_t id) override;
+//   int addFriend(userid_t selfId, userid_t target) override;
+// };
 
 struct Block {
     std::string savePath;
@@ -118,10 +123,10 @@ public:
 
   std::string getUserNickname(userid_t id) override;
 
-  void setMessage(Object object, Encoder& encoder);
-  void addMessage(Object object, Encoder& encoder, chatid_t chatid);
-  Object getMessage(int id, Encoder& encoder);
-  Object getLastMessage(Encoder& encoder, chatid_t chatid);
+  void setMessage(Object object, Encoder& encoder) override;
+  void addMessage(Object object, Encoder& encoder, chatid_t chatid) override;
+  Object getMessage(int id, Encoder& encoder) override;
+  Object getLastMessage(Encoder& encoder, chatid_t chatid) override;
 
 private:
   
